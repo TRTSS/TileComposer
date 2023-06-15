ec13f22454bedaea31aec5fe612ce233cd5e98d1
TRTSS <felix.trof@gmail.com>
2023-06-15 19:00:41 +0300

    threads update

--- musicGeneration/main.py
+++ musicGeneration/main.py
@@ -1,11 +1,13 @@
 import math
+import threading
 import wave
 import struct
+import time
 
 from alive_progress import alive_bar
 
 
-def GetTileCountByPicks(frames, rate, pick, maxTPS):
+def GetTileCountByPicks(frames, rate, pick, maxTPS, threadName):
     frames = [abs(item) for item in frames]
     maxPick = max(frames)
     maxSecPick = max(frames[:rate])
@@ -24,45 +26,47 @@ def GetTileCountByPicks(frames, rate, pick, maxTPS):
     ind = 0
     sec = 0
     tmpTile = []
-    with alive_bar(len(frames), title='Обработка', bar='smooth') as bar:
-        for indexM, i in enumerate(frames):
-            if i > maxPick - pick:
-                tmpTile.append(i)
-            else:
-                tmpTile.append(maxPick + 1)
+    # with alive_bar(len(frames), title=f'Обработка [{threadName}]', bar='smooth') as bar:
+    for indexM, i in enumerate(frames):
+        if i > maxPick - pick:
+            tmpTile.append(i)
+        else:
+            tmpTile.append(maxPick + 1)
 
-            if (ind % rate == 0):
-                # sec += 1
-                # startFrame = sec * rate
-                # endFrame = (sec+1) * rate;
-                # maxSecPick = max(frames[startFrame:endFrame])
-                # print (f"Max pick: {maxSecPick}")
-                # print(f"Проанализировано {round(indexM / len(frames) * 100, 2)}% ({indexM} / {len(frames)} фреймов)")
-                while (len([item for item in tmpTile if item != maxPick + 1]) > maxTPS / 2):
-                    indToDelete = tmpTile.index(min(tmpTile))
-                    tmpTile[indToDelete] = maxPick + 1
+        if (ind % rate == 0):
+            # sec += 1
+            # startFrame = sec * rate
+            # endFrame = (sec+1) * rate;
+            # maxSecPick = max(frames[startFrame:endFrame])
+            # print (f"Max pick: {maxSecPick}")
+            # print(f"Проанализировано {round(indexM / len(frames) * 100, 2)}% ({indexM} / {len(frames)} фреймов)")
+            while (len([item for item in tmpTile if item != maxPick + 1]) > maxTPS / 2):
+                indToDelete = tmpTile.index(min(tmpTile))
+                tmpTile[indToDelete] = maxPick + 1
 
-                for index, item in enumerate(tmpTile):
-                    if (item == maxPick + 1):
-                        tmpTile[index] = 0
-                    else:
-                        tmpTile[index] = 1
+            for index, item in enumerate(tmpTile):
+                if (item == maxPick + 1):
+                    tmpTile[index] = 0
+                else:
+                    tmpTile[index] = 1
 
-                SuitableTiles.extend(tmpTile)
-                tmpTile.clear()
-            ind += 1
-            bar()
+            SuitableTiles.extend(tmpTile)
+            tmpTile.clear()
+        ind += 1
+        if indexM % 10000 == 0:
+            print(f'[{threadName}] => Done with {indexM / len(frames) * 100}%')
+        # bar()
 
-    file = open(f"{input('Имя файла результата: ')}.til", "w")
-
-    print("Запись нот в файл...")
-    for index, item in enumerate(SuitableTiles):
-        if (item == 1):
-            file.write(f"{index / rate / 2}\n")
-
-    file.close()
-
-    print(f"Нот: {SuitableTiles.count(1)}")
+# file = open(f"{input('Имя файла результата: ')}.til", "w")
+#
+# print("Запись нот в файл...")
+# for index, item in enumerate(SuitableTiles):
+#     if (item == 1):
+#         file.write(f"{index / rate / 2}\n")
+#
+# file.close()
+#
+# print(f"Нот: {SuitableTiles.count(1)}")
 
 
 source = wave.open(input('Путь до файла для обработки: '), mode='rb')
@@ -89,7 +93,30 @@ print(f"Макс: {maxFrame}\nМин: {minFrame}\nАмплитуда: ~{(maxFram
 print("-------------НАСТРОЙКА-------------")
 print("-------------ШАБЛОНЫ ПИКОВ-------------")
 
-GetTileCountByPicks(framesList, rate, int(input(f'Максимальный пик ({maxFrame} макс.): ')), int(input('TPS: ')))
+p = int(input(f'Максимальный пик ({maxFrame} макс.): '))
+tps = int(input('TPS: '))
+
+st = time.time()
+
+threadsCount = 5
+framesForThread = len(framesList) // threadsCount
+threads = []
+for i in range(threadsCount):
+    s = framesForThread * i
+    f = framesForThread * (i + 1)
+    threads.append(threading.Thread(target=GetTileCountByPicks, args=(framesList[s:f], rate, p, tps, str(i + 1))))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+et = time.time()
+
+print(f'done with {et - st} sec.')
+
+# GetTileCountByPicks(framesList, rate, p, tps)
 # print (f"Пик 1500; TPS 3 = {GetTileCountByPicks(frames, rate, 1500, 3)} нот")
 # print (f"Пик 2500; TPS 3 = {GetTileCountByPicks(frames, rate, 2500, 3)} нот")
 # print (f"Пик 3500; TPS 3= {GetTileCountByPicks(frames, rate, 3500, 3)} нот")
